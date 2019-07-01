6ba57c2188e1036177ad1272ac98f6042425fcc4
Arshad Khan <arshdkhn1@gmail.com>
2019-07-01 21:37:18 +0530

    Update spinalCase.js

--- front-end-projects/intermidiate-algorithm-scripting/spinalCase.js
+++ front-end-projects/intermidiate-algorithm-scripting/spinalCase.js
@@ -1,31 +1,11 @@
-function spinalCase(str) {
-  let spinalStr = str.replace(/\s|_/g, "-");
-  // adds - in front of every capital letter
-  // Removing first letter as it has not to be prefixed with - if it is a cap 
-  spinalStr = spinalStr.slice(1).replace(/-?([A-Z])/g, "-$1");
+const spinalCase = str => {
+  // 1. replaces spaces and underscores with dash
+  // 2. remove first letter as it has not to be prefixed with dash if it is a cap
+  // 3. adds dash in front of every capital letter
+  let spinalStr = str.replace(/\s|_/g, '-')
+					 .slice(1)
+					 .replace(/-?([A-Z])/g, '-$1');
+
+  // Adding first char in lowerCase + rest of the spinalCased string
   return str[0].toLowerCase() + spinalStr.toLowerCase();
-}
-
-console.log(spinalCase('This_Is Spinal Tap'));
-// function spinalCase(str) {
-//   // "It's such a fine line between stupid, and clever."
-//   // --David St. Hubbins
-
-//   //upperCaseLetters have all the capital letter used in str, will use it later with indexOf
-//   var index = 0;
-//   var upperCaseLetters = str.match(/[A-Z]/g);
-//   //replaces all spaces and underscore with dash
-//   str = str.replace(/_|\s/g,"-");
-//   for(var i = 0; i <= upperCaseLetters.length; i++){
-//     while(str.indexOf(upperCaseLetters[i]) > 0){
-//       index = str.indexOf(upperCaseLetters[i]);
-//       str = str.substring(0,index) + "-" + str[index].toLowerCase() + str.substring(index + 1);
-//      }
-//   }
-//   str = str.toLowerCase();
-//   str = str.replace(/--/g,"-");
-//   return str;
-// }
-
-// spinalCase('This Is Spinal Tap');
-
+};
