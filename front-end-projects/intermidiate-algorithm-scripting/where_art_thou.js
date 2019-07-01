30a6407780f138a842f0ece3daa030e18771cb2b
Arshad Khan <arshdkhn1@gmail.com>
2019-07-01 18:28:34 +0530

    Update where_art_thou.js

--- front-end-projects/intermidiate-algorithm-scripting/where_art_thou.js
+++ front-end-projects/intermidiate-algorithm-scripting/where_art_thou.js
@@ -1,22 +1,15 @@
-
-function whereAreYou(collection, source) {
+function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
   // Only change code below this line
-  collection.forEach(function(obj){
-    var haveAll = true;
-    var keys = Object.keys(source);
-    keys.forEach(function(key){
-      if(!obj.hasOwnProperty(key) || obj[key] !== source[key]){
-       haveAll = false;
-      }
-    });
-    if(haveAll)
-      arr.push(obj);
-  });
-
+  
+  collection.forEach(e => {
+    const sourceKeys = Object.keys(source);
+    const matchFound = sourceKeys.every(key => e[key] === source[key]);
+    if(matchFound) arr.push(e);
+  })
   // Only change code above this line
   return arr;
 }
 
-whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
