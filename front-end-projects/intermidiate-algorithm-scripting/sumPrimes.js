22dc1c11662b8872efec4b9acfefbb3d3a48ef7c
Arshad Khan <arshdkhn1@gmail.com>
2019-07-02 00:10:31 +0530

    Update sumPrimes.js

--- front-end-projects/intermidiate-algorithm-scripting/sumPrimes.js
+++ front-end-projects/intermidiate-algorithm-scripting/sumPrimes.js
@@ -1,24 +1,10 @@
-function sumPrimes(num) {
-var prime;
-var number = 0;
-  if (num > 1){
-  for (i = 2; i <= num; i++) {
-
-    prime = true;
-
-    for (j = 2; j <= i/2; j ++){
-
-      if (i % j === 0){
-        prime = false;
-      }
-
-    }
-    if (prime === true){
-     number = number + i;
-    }
+const isPrime = num => {
+  for(let i = 2; i <= parseInt(num/2); i++) {
+    if(num % i === 0) return false;
   }
-  }
-  return number;
-}
+  return true;
+};
 
-sumPrimes(10);
+const sumPrimes = num => Array(num - 1).fill().map((e, i) => num - i).reduce((t, e) => t + (isPrime(e) ? e : 0), 0)
+
+console.log(sumPrimes(10));
