8fee30edb25de2ad09f9db8dd6100ff080b657f8
Arshad Khan <arshdkhn1@gmail.com>
2019-07-01 21:54:35 +0530

    Update dna_pairing.js

--- front-end-projects/intermidiate-algorithm-scripting/dna_pairing.js
+++ front-end-projects/intermidiate-algorithm-scripting/dna_pairing.js
@@ -1,35 +1,3 @@
-
-function pairElement(str) {
-  var newStr= [];
-  str = str.toUpperCase();
-  for(var i = 0; i < str.length; i++){
-    switch(str[i]){
-
-      case 'A':
-        newStr[i]=[];
-        newStr[i].push(str[i]);
-        newStr[i].push('T');
-        break;
-      case 'T':
-        newStr[i]=[];
-        newStr[i].push(str[i]);
-        newStr[i].push('A');
-        break;
-      case 'G':
-        newStr[i]=[];
-        newStr[i].push(str[i]);
-        newStr[i].push('C');
-        break;
-      case 'C':
-        newStr[i]=[];
-        newStr[i].push(str[i]);
-        newStr[i].push('G');
-        break;
-      default :
-        return null;
-    }
-  }
-  return newStr;
-}
-
-pairElement("GCG");
+const pairElement = 
+  (str, pairs = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}) =>
+  str.split``.map(e => [e, pairs[e]])
