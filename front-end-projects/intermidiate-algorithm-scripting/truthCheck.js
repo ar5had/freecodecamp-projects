5c86eec289b51d6ba7aac066bdec990037880f8d
Arshad Khan <arshdkhn1@gmail.com>
2019-07-02 15:56:47 +0530

    Update truthCheck.js

--- front-end-projects/intermidiate-algorithm-scripting/truthCheck.js
+++ front-end-projects/intermidiate-algorithm-scripting/truthCheck.js
@@ -1,11 +1,2 @@
-
-function truthCheck(collection, pre) {
-  // Is everyone being true?
-  for(var elem in collection){
-    if( !(collection[elem].hasOwnProperty(pre) && Boolean(collection[elem][pre]) ) )
-      return false;
-  }
-  return true;
-}
-
+const truthCheck = (collection, pre) => collection.every(e => Boolean(e[pre]));
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
