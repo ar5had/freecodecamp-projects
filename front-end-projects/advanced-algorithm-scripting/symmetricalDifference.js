2e0bdc4c28811df41e61c639ec41c953975a35a3
Arshad Khan <arshdkhn1@gmail.com>
2019-07-03 00:42:18 +0530

    Update symmetricalDifference.js

--- front-end-projects/advanced-algorithm-scripting/symmetricalDifference.js
+++ front-end-projects/advanced-algorithm-scripting/symmetricalDifference.js
@@ -1,21 +1,21 @@
 const symDiff = (a, b) => a.filter(e => !b.includes(e)).concat(b.filter(e => !a.includes(e)));
 
-// more efficient than symDiff
+// efficient symDiff
 const effSymDiff = (a, b) => {
-	const diff = [];
-  	for(let i = 0; i < Math.max(a.length, b.length); i++) {
-    	const aElem = a[i];
-      	const bElem = b[i];
-      
-     	if(aElem && !b.includes(aElem)) {
-          	diff.push(aElem);
+    const diff = [];
+    for (let i = 0; i < Math.max(a.length, b.length); i++) {
+        const aElem = a[i];
+        const bElem = b[i];
+
+        if (aElem && !b.includes(aElem)) {
+            diff.push(aElem);
         }
-      
-      	if(bElem && !a.includes(bElem)) {
-        	diff.push(bElem);
+
+        if (bElem && !a.includes(bElem)) {
+            diff.push(bElem);
         }
     }
-	return diff;
+    return diff;
 };
 
 const symDiffArr = (...arr) => arr.reduce(symDiff);
