e3166046c55b3d76cf2cfccd6cb38ae359e0f079
Arshad Khan <arshdkhn1@gmail.com>
2019-07-02 15:33:12 +0530

    Update flatteningArray.js

--- front-end-projects/intermidiate-algorithm-scripting/flatteningArray.js
+++ front-end-projects/intermidiate-algorithm-scripting/flatteningArray.js
@@ -11,3 +11,36 @@ function steamrollArray(arr) {
   return flattenArray;
 }
 steamrollArray([1, [2], [3, [[4]]]]);
+
+
+// Take 2
+
+const flattenArray = (elem, memoArr = []) => {
+  // Base case - if elem is not an array then push it on to the memoized array
+  if(!Array.isArray(elem)) {
+    memoArr.push(elem);
+  } else {
+    elem.forEach(subElem => {
+			flattenArray(subElem, memoArr)
+		});
+  }
+	
+	return memoArr;
+}
+
+const steamrollArray = arr => {
+  let newArr = [];
+
+  arr.forEach(elem => {
+    if(Array.isArray(elem)) {
+      newArr = [...newArr, ...flattenArray(elem)];
+    } else {
+      newArr.push(elem);
+    }
+  });
+
+  return newArr;
+};
+
+const results = steamrollArray([1, [2], [3, 5]]);
+console.log(JSON.stringify(results));
