6577a1f0e74c7eecb92dd8b27738de14e452f43e
Arshad Khan <arshdkhn1@gmail.com>
2019-07-02 15:52:13 +0530

    Update binaryAgent.js

--- front-end-projects/intermidiate-algorithm-scripting/binaryAgent.js
+++ front-end-projects/intermidiate-algorithm-scripting/binaryAgent.js
@@ -1,11 +1,3 @@
-
-function binaryAgent(str) {
-  str = str.split(" ");
-  for(var elem in str){
-    str[elem] = String.fromCharCode(parseInt(str[elem], 2));
-  }
-  str = str.join("");
-  return str;
-}
+const binaryAgent = str => str.split` `.map(e => String.fromCharCode(parseInt(e, 2))).join``;
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
