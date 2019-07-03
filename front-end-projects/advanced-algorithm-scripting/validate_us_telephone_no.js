d7c27d1e929db13b79d81e677fc413b3240574f2
Arshad Khan <arshdkhn1@gmail.com>
2019-07-03 23:58:07 +0530

    Update validate_us_telephone_no.js

--- front-end-projects/advanced-algorithm-scripting/validate_us_telephone_no.js
+++ front-end-projects/advanced-algorithm-scripting/validate_us_telephone_no.js
@@ -1,53 +1,12 @@
+const telephoneCheck = str => {
+  // Regex explained
+  // ^  Looks for the starting string
+  // (1\s?)? - matches if string is "1 " or "1" [OPTIONAL]
+  // ((\(\d{3}\))|\d{3}) - matches if string is "(999)" or "999"
+  // (-|\s)? - matches if string is "-" or " " [OPTIONAL]
+  // \d{4}$ - matches if ending string is "9999"
+  const regex = /^(1\s?)?((\(\d{3}\))|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/;
+  return regex.test(str)
+};
 
-function telephoneCheck(str) {
-  // Good luck!
-    var check = str.match(/\d/g);
-
-  var noArray= str.split("");
-  var len = noArray.length;
-  if( (len === 10 || len === 12 || len === 13 || len === 14 || len === 16) && (check.length === 10 || check.length === 11) ){
-
-    if(len === 10)
-      return true;
-
-    else if(len === 12){
-      if(noArray[3] === '-' && noArray[7] === '-')
-        return true;
-      else if(noArray[3] === ' ' && noArray[7] === ' ')
-        return true;
-
-    }
-
-    else if(len === 13){
-      if(noArray[0] === '(' && noArray[4] === ')' && noArray[8] === '-')
-        return true;
-
-    }
-
-    else if(len === 14){
-      if(noArray[0] === '(' && noArray[4] === ')' && noArray[5] === ' ' && noArray[9] === '-')
-        return true;
-      else if(noArray[0] === '1' && noArray[1] === ' ' && noArray[5] === ' ' && noArray[9] === ' ')
-        return true;
-      else if(noArray[0] === '1' && noArray[1] === ' ' && noArray[5] === '-' && noArray[9] === '-')
-        return true;
-      else if(noArray[0] === '1' && noArray[1] === '(' && noArray[5] === ')' && noArray[9] === '-')
-        return true;
-
-    }
-
-    else if(len === 16){
-      if(noArray[0] === '1' && noArray[1] === ' ' && noArray[2] === '(' && noArray[6] === ')' &&
-        noArray[7] === ' ' && noArray[11] === '-')
-        return true;
-
-    }
-
-  }
-
-  return false;
-}
-
-
-
-telephoneCheck("555-555-5555");
+telephoneCheck("1 456 789 4444");
